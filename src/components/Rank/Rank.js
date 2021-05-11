626f9b4da727921260af437135d5b390c8430f2c
Dani Dima <dani.emrah@gmail.com>
2021-05-11 23:38:22 +0300

    sadsa

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () =>{
+const Rank = ({name, entries}) =>{
     return(
         <div>
             <div className="white f3 center">
-                {"Your curent rank is: "}
+                {`${name} curent entry count is: `}
             </div>
             <div className="white f1 center">
-                {"#5"}
+                {`${entries}`}
             </div>
         </div>
     )
---
+++ src/components/Register/Register.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+
+const Register = ({ onRouteChange, loadUser }) => {
+  const [newName, setNewName] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  const onNewNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const onNewEmailChange = (event) => {
+    setNewEmail(event.target.value);
+  };
+
+  const onNewPasswordChange = (event) => {
+    setNewPassword(event.target.value);
+  };
+
+  const onSubmitRegister = () => {
+    if (newName === '' || newEmail === '' || newPassword === '') {
+    } else {
+      fetch('http://localhost:3000/register', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: newEmail,
+          password: newPassword,
+          name: newName,
+        }),
+      })
+        .then((response) => response.json())
+        .then((user) => {
+          if (user) {
+            loadUser(user);
+            onRouteChange('home');
+          }
+        });
+    }
+  };
+
+  return (
+    <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <main className="pa4 black-80">
+        <div className="measure ">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="name">
+                Name
+              </label>
+              <input
+                onChange={onNewNameChange}
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="text"
+                name="name"
+                id="name"
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Email
+              </label>
+              <input
+                onChange={onNewEmailChange}
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" for="password">
+                Password
+              </label>
+              <input
+                onChange={onNewPasswordChange}
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+              />
+            </div>
+          </fieldset>
+          <div className="">
+            <input
+              onClick={onSubmitRegister}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Register"
+            />
+          </div>
+        </div>
+      </main>
+    </article>
+  );
+};
+
+export default Register;
